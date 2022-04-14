db899a312898850f17e8c49bab5b96c5b3358f51
GeleVla <schoorl.arjan@outlook.com>
2022-04-14 12:24:27 +0200

    Added Bstats

--- src/main/java/com/gelevla/whitelistbypass/WhitelistBypass.java
+++ src/main/java/com/gelevla/whitelistbypass/WhitelistBypass.java
@@ -8,6 +8,8 @@ import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import net.milkbowl.vault.permission.Permission;
+import org.bstats.bukkit.Metrics;
+
 
 public final class WhitelistBypass extends JavaPlugin implements Listener{
 
@@ -19,6 +21,9 @@ public final class WhitelistBypass extends JavaPlugin implements Listener{
     public void onEnable() {
         this.setupPermissions();
         Bukkit.getPluginManager().registerEvents(this, this);
+
+        int pluginId = 14924; // <-- Replace with the id of your plugin!
+        Metrics metrics = new Metrics(this, pluginId);
     }
 
     private boolean setupPermissions() {
